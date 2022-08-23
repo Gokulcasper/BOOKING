16dd4c22b9525ab0f24f49b3e2bf566ee3b90a43
Gokulcasper <ronitcasper215@gmail.com>
2022-08-23 17:29:24 +0530

    Feat : Complete propertyList

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,43 @@
+import "./featured.css";
+
+const Featured = () => {
+  return (
+    <div className="featured">
+      <div className="featuredItem">
+        <img
+          src="https://www.tamilnadutourism.com/images/tour-packages/card/gangaikondacholapuram.jpg"
+          alt=""
+          className="featuredImg"
+        />
+        <div className="featuredTitles">
+          <h1>TamilNadu</h1>
+          <h2>123 Properties</h2>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img
+          src="https://static.toiimg.com/thumb/msid-90495077,width-748,height-499,resizemode=4,imgsize-178792/.jpg"
+          alt=""
+          className="featuredImg"
+        />
+        <div className="featuredTitles">
+          <h1>Kerala</h1>
+          <h2>125 Properties</h2>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img
+          src="https://static.toiimg.com/thumb/msid-81460131,width-748,height-499,resizemode=4,imgsize-1147454/.jpg"
+          alt=""
+          className="featuredImg"
+        />
+        <div className="featuredTitles">
+          <h1>Karnataka</h1>
+          <h2>124 Properties</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
