4693494da242d0fedac7edbe01986e5dc03786f5
Gokulcasper <ronitcasper215@gmail.com>
2022-09-09 17:00:45 +0530

    Feat : Added FeaturedProperties

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -16,7 +16,7 @@ const FeaturedProperties = () => {
               <span className="fpName">{item.name}</span>
               <span className="fpCity">{item.city}</span>
               <span className="fpPrice">
-                Starting From ${item.cheapestPrice}
+                Starting from ${item.cheapestPrice}
               </span>
               {item.rating && (
                 <div className="fpRating">
